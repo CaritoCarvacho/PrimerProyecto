6b3e14ca9e763d976c09198d9ccbd93d4158db91
CaritoCarvacho <flga.carolinacarvacho@gmail.com>
2023-07-18 00:05:31 -0300

    Sorter

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -3,9 +3,9 @@ import { GET_POKEMONS, GET_TYPES } from "./actionTypes";
 
 
 
-export const getPokemons = ({currentPage}) => {
+export const getPokemons = ({currentPage, sortBy, sortOrder}) => {
     return async function (dispatch) {
-        const apiData = await axios.get(`http://localhost:3001/pokemons?pageNumber=${currentPage}`);
+        const apiData = await axios.get(`http://localhost:3001/pokemons?pageNumber=${currentPage}&sortBy=${sortBy}&sortOrder=${sortOrder}`);
         const pokemons = apiData.data;
         dispatch({ type: GET_POKEMONS, payload: pokemons });
     };
--- client/src/views/homePage/home.component.jsx
+++ client/src/views/homePage/home.component.jsx
@@ -5,21 +5,24 @@ import {useSelector, useDispatch} from "react-redux";
 import { getPokemons } from "../../redux/actions/actions";
 import Loading from "../../components/Loading/Loading.components";
 import Paginator from "../../components/Paginator/Paginator.components";
+import Sorter from "../../components/Sorter/Sorter.components";
 
 function Home() {
   const dispatch = useDispatch();
+  const [ loading, setLoading ] = useState(false);
   const { pokemons, types } = useSelector((state) => state);
-  const [currentPage, setCurrentPage] = useState(0);
-  const [loading, setLoading] = useState(false);
+  const [ currentPage, setCurrentPage ] = useState(0);
+  const [ sortBy, setSortBy ] = useState('name');
+  const [ sortOrder, setSortOrder ] = useState('asc');
 
   useEffect(() => {
     setLoading(false);
   }, [pokemons]);
 
   useEffect(() => {
-    dispatch(getPokemons({currentPage}));
+    dispatch(getPokemons({currentPage, sortBy, sortOrder}));
     setLoading(true);
-  }, [currentPage]);
+  }, [currentPage, sortBy, sortOrder]);
 
   if (loading) {
     return (<Loading/>)
@@ -28,6 +31,7 @@ function Home() {
   return (
     <div>
       <p>Home Page</p>
+        <Sorter sortBy={sortBy} sortOrder={sortOrder} onChangeBy={setSortBy} onChangeOrder={setSortOrder} />
         <Paginator totalPages={pokemons.totalPages} onClick={(page) => setCurrentPage(page)}></Paginator>
         <CardsContainer pokemons={pokemons} index={0} />
         <Paginator totalPages={pokemons.totalPages} onClick={(page) => setCurrentPage(page)}></Paginator>
